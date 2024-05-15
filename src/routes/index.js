987a500975a3a239df0fed9c281cd51ddd60ac02
hieungocdinh <hieungocdinh2003@gmail.com>
2024-05-15 08:40:03 +0700

    update 15/5/24

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+// Import layout
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+
+// những route không đăng nhập cũng vào được
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+//những route phải đăng nhâp mới vào được
+const privateRoutes = [
+
+];
+
+export { publicRoutes, privateRoutes };
